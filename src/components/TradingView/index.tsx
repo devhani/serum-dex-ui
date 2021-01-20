2b81409303f04c3cf11bf7447f999eddda0410b4
devhani <ajrulla.jasin@gmail.com>
2021-01-20 03:33:12 +0100

    Update index.tsx

--- src/components/TradingView/index.tsx
+++ src/components/TradingView/index.tsx
@@ -1,86 +1,98 @@
 import * as React from 'react';
 import './index.css';
-import { widget } from '../charting_library/charting_library.min';
+import {
+  widget,
+  ChartingLibraryWidgetOptions,
+  IChartingLibraryWidget,
+} from '../../charting_library'; // Make sure to follow step 1 of the README
+import { useMarket } from '../../utils/markets';
+import { BONFIDA_DATA_FEED } from '../../utils/bonfidaConnector';
+import { findTVMarketFromAddress } from '../../utils/tradingview';
 
-function getLanguageFromURL() {
-	const regex = new RegExp('[\\?&]lang=([^&#]*)');
-	const results = regex.exec(window.location.search);
-	return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
+// This is a basic example of how to create a TV widget
+// You can add more feature such as storing charts in localStorage
+
+export interface ChartContainerProps {
+  symbol: ChartingLibraryWidgetOptions['symbol'];
+  interval: ChartingLibraryWidgetOptions['interval'];
+  datafeedUrl: string;
+  libraryPath: ChartingLibraryWidgetOptions['library_path'];
+  chartsStorageUrl: ChartingLibraryWidgetOptions['charts_storage_url'];
+  chartsStorageApiVersion: ChartingLibraryWidgetOptions['charts_storage_api_version'];
+  clientId: ChartingLibraryWidgetOptions['client_id'];
+  userId: ChartingLibraryWidgetOptions['user_id'];
+  fullscreen: ChartingLibraryWidgetOptions['fullscreen'];
+  autosize: ChartingLibraryWidgetOptions['autosize'];
+  studiesOverrides: ChartingLibraryWidgetOptions['studies_overrides'];
+  containerId: ChartingLibraryWidgetOptions['container_id'];
+  theme: string;
 }
 
-export class TVChartContainer extends React.PureComponent {
-	static defaultProps = {
-		symbol: 'AAPL',
-		interval: 'D',
-		containerId: 'tv_chart_container',
-		datafeedUrl: 'https://demo_feed.tradingview.com',
-		libraryPath: '/charting_library/',
-		chartsStorageUrl: 'https://saveload.tradingview.com',
-		chartsStorageApiVersion: '1.1',
-		clientId: 'tradingview.com',
-		userId: 'public_user_id',
-		fullscreen: false,
-		autosize: true,
-		studiesOverrides: {},
-	};
+export interface ChartContainerState {}
 
-	tvWidget = null;
+export const TVChartContainer = () => {
+  // @ts-ignore
+  const defaultProps: ChartContainerProps = {
+    symbol: 'BTC/USDC',
+    interval: '60',
+    theme: 'Dark',
+    containerId: 'tv_chart_container',
+    datafeedUrl: BONFIDA_DATA_FEED,
+    libraryPath: '/charting_library/',
+    fullscreen: false,
+    autosize: true,
+    studiesOverrides: {},
+  };
 
-	componentDidMount() {
-		const widgetOptions = {
-			symbol: this.props.symbol,
-			// BEWARE: no trailing slash is expected in feed URL
-			datafeed: new window.Datafeeds.UDFCompatibleDatafeed(this.props.datafeedUrl),
-			interval: this.props.interval,
-			container_id: this.props.containerId,
-			library_path: this.props.libraryPath,
+  const tvWidgetRef = React.useRef<IChartingLibraryWidget | null>(null);
+  const { market } = useMarket();
 
-			locale: getLanguageFromURL() || 'en',
-			disabled_features: ['use_localstorage_for_settings'],
-			enabled_features: ['study_templates'],
-			charts_storage_url: this.props.chartsStorageUrl,
-			charts_storage_api_version: this.props.chartsStorageApiVersion,
-			client_id: this.props.clientId,
-			user_id: this.props.userId,
-			fullscreen: this.props.fullscreen,
-			autosize: this.props.autosize,
-			studies_overrides: this.props.studiesOverrides,
-		};
+  React.useEffect(() => {
+    const widgetOptions: ChartingLibraryWidgetOptions = {
+      symbol: findTVMarketFromAddress(
+        market?.address.toBase58() || '',
+      ) as string,
+      // BEWARE: no trailing slash is expected in feed URL
+      // tslint:disable-next-line:no-any
+      datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(
+        defaultProps.datafeedUrl,
+      ),
+      interval: defaultProps.interval as ChartingLibraryWidgetOptions['interval'],
+      container_id: defaultProps.containerId as ChartingLibraryWidgetOptions['container_id'],
+      library_path: defaultProps.libraryPath as string,
+      locale: 'en',
+      disabled_features: ['use_localstorage_for_settings'],
+      enabled_features: ['study_templates'],
+      load_last_chart: true,
+      client_id: defaultProps.clientId,
+      user_id: defaultProps.userId,
+      fullscreen: defaultProps.fullscreen,
+      autosize: defaultProps.autosize,
+      studies_overrides: defaultProps.studiesOverrides,
+      theme: 'Dark',
+    };
 
-		const tvWidget = new widget(widgetOptions);
-		this.tvWidget = tvWidget;
+    const tvWidget = new widget(widgetOptions);
+    tvWidgetRef.current = tvWidget;
 
-		tvWidget.onChartReady(() => {
-			tvWidget.headerReady().then(() => {
-				const button = tvWidget.createButton();
-				button.setAttribute('title', 'Click to show a notification popup');
-				button.classList.add('apply-common-tooltip');
-				button.addEventListener('click', () => tvWidget.showNoticeDialog({
-					title: 'Notification',
-					body: 'TradingView Charting Library API works correctly',
-					callback: () => {
-						console.log('Noticed!');
-					},
-				}));
+    tvWidget.onChartReady(() => {
+      tvWidget.headerReady().then(() => {
+        const button = tvWidget.createButton();
+        button.setAttribute('title', 'Click to show a notification popup');
+        button.classList.add('apply-common-tooltip');
+        button.addEventListener('click', () =>
+          tvWidget.showNoticeDialog({
+            title: 'Notification',
+            body: 'TradingView Charting Library API works correctly',
+            callback: () => {
+              console.log('It works!!');
+            },
+          }),
+        );
+        button.innerHTML = 'Check API';
+      });
+    });
+  }, [market]);
 
-				button.innerHTML = 'Check API';
-			});
-		});
-	}
-
-	componentWillUnmount() {
-		if (this.tvWidget !== null) {
-			this.tvWidget.remove();
-			this.tvWidget = null;
-		}
-	}
-
-	render() {
-		return (
-			<div
-				id={ this.props.containerId }
-				className={ 'TVChartContainer' }
-			/>
-		);
-	}
-}
+  return <div id={defaultProps.containerId} className="tradingview-chart" />;
+};
